1da23ab717586a59f3ba7ddbadaca595b6a6ba9d
齋藤 大之 <dai@Chika.local>
2013-02-05 14:58:56 +0900

    Add main program.

--- valid_mail_checker.rb
+++ valid_mail_checker.rb
@@ -7,5 +7,36 @@
 # 2) DO NOT use for too many email adresses. SMTP server will block you and networks.
 
 require 'net/smtp'
+require 'resolv'
 
+HELO = "localhost"
+MAILFROM = "info@localhost"
+
+mail_adress = ARGV[0]
+unless mail_adress =~ /^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/
+  puts "This mail adress is not valid syntax."
+  exit
+end
+mail_domain = (mail_adress.split '@')[1]
+
+resolver = Resolv::DNS.new
+mx_domain = (resolver.getresource mail_domain, Resolv::DNS::Resource::IN::MX).exchange.to_s
+
+Net::SMTP.start mx_domain, 25, HELO do |smtp|
+  begin
+    smtp.mailfrom MAILFROM
+  rescue
+    puts "Server retern error at MAIL FROM."
+    exit
+  end
+
+  begin
+    smtp.rcptto mail_adress
+  rescue
+    puts "#{mail_adress} is not exist."
+    exit
+  end
+end
+
+puts "#{mail_adress} seems to be exist."
 
