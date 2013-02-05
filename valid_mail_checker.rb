3d85298330493a61797ec6891b9cf58e2595fcc3
齋藤 大之 <dai@Chika.local>
2013-02-05 14:14:50 +0900

    Add simple document.

---
+++ valid_mail_checker.rb
@@ -0,0 +1,11 @@
+# Valid mail checker
+#
+# This program is a tool for checking mail adresses are valid or not.
+#
+# !!CAUTION!!
+# 1) DO NOT use for spaming or other bad things.
+# 2) DO NOT use for too many email adresses. SMTP server will block you and networks.
+
+require 'net/smtp'
+
+
